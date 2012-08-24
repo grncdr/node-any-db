09a137daf90e3297e104cf652d705cfc39571bd1
Stephen Sugden <glurgle@gmail.com>
2012-08-23 23:14:59 -0700

    lower pool idle timeout in pool test helpers

--- test/helpers.js
+++ test/helpers.js
@@ -1,17 +1,18 @@
 anyDB = require('../')
 test = require('tap').test
+require('sqlite3').verbose()
 
 databaseUrls = {
-  mysql: "mysql://root@localhost/any_db_test",
-  sqlite3: "sqlite3://:memory:",
-  postgres: "postgres://postgres@localhost/any_db_test"
+	mysql: "mysql://root@localhost/any_db_test",
+	sqlite3: "sqlite3://:memory:",
+	postgres: "postgres://postgres@localhost/any_db_test"
 }
 
 exports.allDrivers = function (description, callback) {
-  /*
-  Run ``callback(pool, tap_test)`` where ``pool`` is a connection to the test
-  database, and ``tap_test`` is a node-tap test object
-  */
+	/*
+	Run ``callback(pool, tap_test)`` where ``pool`` is a connection to the test
+	database, and ``tap_test`` is a node-tap test object
+	*/
 	_testEachDriver(description, function (connString, t) { 
 		anyDB.createConnection(connString, function (err, conn) {
 			if (err) throw err
@@ -22,15 +23,20 @@ exports.allDrivers = function (description, callback) {
 }
 
 exports.allPools = function (description, callback) {
-  /*
-  Run ``callback(pool, tap_test)`` where ``pool`` is a connection pool
+	/*
+	Run ``callback(pool, tap_test)`` where ``pool`` is a connection pool
 	that will connect to the test database and ``tap_test`` is a node-tap test
 	object.
-  */
-  var dbname = 'db_any_test'
-    , i = 0;
+	*/
+	var dbname = 'db_any_test'
+		, i = 0;
 	_testEachDriver(description, function (connString, t) {
-		var pool = anyDB.getPool(connString, {max: 2})
+		var pool = anyDB.createPool(connString, {
+			max: 2,
+			min: 0,
+			idleTimoutMillis: 1000
+		})
+		t.on('end', pool.close.bind(pool))
 		callback(pool, t)
 	})
 }
@@ -38,7 +44,7 @@ exports.allPools = function (description, callback) {
 function _testEachDriver (description, callback) {
 	test(description, function (outer_t) {
 		Object.keys(databaseUrls).forEach(function (driverName) {
-			outer_t.test(driverName, function (t) {
+			outer_t.test(description + ' - ' + driverName, function (t) {
 				callback(databaseUrls[driverName], t)
 			})
 		})
