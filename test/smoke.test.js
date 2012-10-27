7b8c8f0abe8d1c591448bca134f3bf5bbdf54870
Stephen Sugden <glurgle@gmail.com>
2012-10-27 15:29:19 -0700

    Fix deepEqual order in test

--- test/smoke.test.js
+++ test/smoke.test.js
@@ -4,7 +4,7 @@ testHelpers.allDrivers('Select 1', function (conn, t) {
 	t.plan(2)
 	conn.query('SELECT 1 AS ok', function (err, res) {
 		t.assert(!err, "No error")
-		t.deepEqual(res, [{ok: 1}], "Got expected result")
+		t.deepEqual(res, [{ok: 1}])
 	})
 })
 
@@ -12,6 +12,6 @@ testHelpers.allPools('Select 1 against pool', function (pool, t) {
 	t.plan(2)
 	pool.query('SELECT 1 AS ok', function (err, res) {
 		t.assert(!err, 'No error')
-		t.deepEqual([{ok: 1}], res)
+		t.deepEqual(res, [{ok: 1}])
 	})
 })
