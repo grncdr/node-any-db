1671758f1ed3459cc92e918fbdc82b16afaf333e
Jens Finkhaeuser <jens@finkhaeuser.de>
2013-11-27 18:55:55 +0100

    Make tests that branch on drivers use the adapter field.

--- test/last_insert_id.test.js
+++ test/last_insert_id.test.js
@@ -4,9 +4,9 @@ require('./helpers').allDrivers("Last insert id",
     t.plan(2)
 
     conn.query("DROP TABLE last_insert_id_test", function (err) {})
-    if (t.conf.name == 'sqlite3') 
+    if (conn.adapter == 'sqlite3') 
       conn.query("CREATE TABLE last_insert_id_test (id integer primary key autoincrement, a int)")
-    else if (t.conf.name == 'mysql')
+    else if (conn.adapter == 'mysql')
       conn.query("CREATE TABLE last_insert_id_test (id integer primary key auto_increment, a int)")
 
     conn.query('INSERT INTO last_insert_id_test (a) VALUES (123)', function (err, res) {
