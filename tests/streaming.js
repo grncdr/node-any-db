b8533ec9041d35e1dfd50e6d879c460ba50ec8f6
Stephen Sugden <me@stephensugden.com>
2013-12-20 20:02:59 -0700

    move transaction tests into any-db-transaction repo

--- tests/streaming.js
+++ tests/streaming.js
@@ -4,7 +4,7 @@ require('../test').withTransaction("Streaming results", function (tx, test) {
   tx.query("DROP TABLE IF EXISTS streaming_test", function (err) { /* swallow errors */ })
   tx.query("CREATE TABLE streaming_test (a int)")
 
-  var placeHolder = tx.url.match(/postgres/) ? '($1)' : '(?)';
+  var placeHolder = tx.adapter == 'postgres' ? '($1)' : '(?)';
   var vals = []
   for (var i = 0; i < 10; i++) {
     tx.query('INSERT INTO streaming_test (a) VALUES ' + placeHolder, [i])
