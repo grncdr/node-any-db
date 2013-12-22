72738138f95496df9d2730fbffc052477c9e7f67
Stephen Sugden <me@stephensugden.com>
2013-12-21 20:40:06 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+var pg = require('pg')
+  , Transaction = require('any-db-transaction')
+  , pgNative = null
+
+try { pgNative = pg.native } catch (e) {}
+
+exports.forceJS = false
+exports.name = 'postgres'
+
+exports.createConnection = function (opts, callback) {
+  var backend = chooseBackend()
+    , conn = new backend.Client(opts)
+
+  conn.adapter = 'postgres'
+  conn.begin = begin
+
+  if (callback) {
+    conn.connect(function (err) {
+      if (err) callback(err)
+      else callback(null, conn)
+    })
+  } else {
+    conn.connect()
+  }
+
+  return conn
+}
+
+// Create a Query object that conforms to the Any-DB interface
+exports.createQuery = function (stmt, params, callback) {
+  var backend = chooseBackend()
+  return new backend.Query(stmt, params, callback)
+}
+
+exports.createTransaction = Transaction.createFromArgs.bind(null,
+                                                            exports.createQuery)
+
+function chooseBackend () {
+  return (exports.forceJS || !pgNative) ? pg : pgNative
+}
+
+function begin (beginStatement, callback) {
+  return exports.createTransaction(beginStatement, callback).setConnection(this)
+}
