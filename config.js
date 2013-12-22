f3a8e5d7c728eda5ccf65a1c6bf120db9ed06aed
Stephen Sugden <me@stephensugden.com>
2013-12-21 21:01:24 -0700

    remove default adapter urls

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ var path = require('path')
 var camelize = require('camelize')
 var cc = require('config-chain')
 var argv = require('optimist').argv
+var parseDbUrl = require('parse-db-url')
 
 var config = cc(camelize(argv),
                 camelize(cc.env('any_db_test_')),
@@ -12,18 +13,11 @@ var adapterPath = path.resolve(config.get('adapterPath'))
 config.set('adapterPath', adapterPath)
 config.set('adapter', require(adapterPath))
 
-if (!config.get('url')) {
-  switch (true) {
-    case /mysql/.test(adapterPath):
-      config.set('url', "mysql://root@localhost/any_db_test")
-    break
-    case /postgres/.test(adapterPath):
-      config.set('url', "postgres://postgres@localhost/any_db_test")
-    break
-    case /sqlite3/.test(adapterPath):
-      config.set('url', "sqlite3:///tmp/any-db-test.db")
-    break
-  }
+var url = config.get('url')
+if (!url) {
+  config.set('url', {})
+} else if (typeof url == 'string') {
+  config.set('url', parseDbUrl(config.get('url')))
 }
 
 module.exports = config.store
