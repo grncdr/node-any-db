48222a1073da417afb5d485d3ee874231c5171e1
Stephen Sugden <me@stephensugden.com>
2013-12-21 19:37:12 -0700

    remove code that belongs to any-db-pool

--- index.js
+++ index.js
@@ -18,25 +18,7 @@ exports.createConnection = function connect (dbUrl, callback) {
 }
 
 exports.createPool = function createPool (dbUrl, poolConfig) {
-  poolConfig = poolConfig || {}
-  if (poolConfig.create || poolConfig.destroy) {
-    throw new Error(
-      "Use onConnect/reset options instead of create/destroy."
-    )
-  }
   var adapterConfig = parseDbUrl(dbUrl);
-  
-  if (adapterConfig.adapter === 'sqlite3' && /:memory:$/i.test(adapterConfig.database)) {
-    if (poolConfig.min > 1 || poolConfig.max > 1) {
-      console.warn(
-        "Pools of more than 1 connection do not work for in-memory SQLite3 databases\n" +
-        "The specified minimum (%d) and maximum (%d) connections have been overridden"
-      )
-    }
-    if (poolConfig.min) poolConfig.min = 1
-    poolConfig.max = 1
-  }
-  
   var adapter = getAdapter(adapterConfig.adapter)
   return new ConnectionPool(adapter, adapterConfig, poolConfig)
 }
