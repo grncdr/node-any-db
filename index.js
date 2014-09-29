adb7c71979ee5f7a0adaef03b667c91cf9ddf4e5
Stephen Sugden <me@stephensugden.com>
2014-09-29 23:57:47 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+exports = module.exports = createParameterContext;
+
+exports.prefixed = require('./lib/prefixed');
+
+exports.adapters = {};
+exports.adapters.postgres = require('./lib/postgres');
+exports.adapters.mssql = require('./lib/mssql');
+exports.adapters.mysql = require('./lib/mysql');
+exports.adapters.sqlite3 = require('./lib/sqlite3');
+
+
+function createParameterContext (queryable) {
+  var anyDbAdapter = queryable.adapter;
+  if (!(anyDbAdapter && anyDbAdapter.name)) {
+    throw new TypeError('Argument must be a valid Queryable');
+  }
+  var impl = exports.adapters[anyDbAdapter.name];
+
+  if (!impl) {
+    throw new Error('The ' + anyDbAdapter.name + ' adapter is not yet supported');
+  }
+
+  return impl();
+}
