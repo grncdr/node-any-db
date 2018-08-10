61ba66525da651a3f778f277d18876c75d47d320
Stephen Sugden <me@stephensugden.com>
2018-08-10 16:07:01 +0200

    Remove any-db-adapter-spec

--- packages/any-db-adapter-spec/interfaces/adapter.js
+++
@@ -1,9 +0,0 @@
-exports.testProperties = function(adapter, assert) {
-  assert.equal(typeof adapter.name, 'string', 'adapter.name is a string')
-  assert.equal(
-    typeof adapter.createConnection,
-    'function',
-    'adapter.createConnection is a function',
-  )
-  assert.equal(typeof adapter.createQuery, 'function', 'adapter.createQuery is a function')
-}
--- packages/any-db-adapter-spec/interfaces/index.js
+++
@@ -1,5 +0,0 @@
-exports.Adapter = require('./adapter')
-exports.Query = require('./query')
-exports.Queryable = require('./queryable')
-exports.Connection = require('./connection')
-exports.ResultSet = require('./result-set')
